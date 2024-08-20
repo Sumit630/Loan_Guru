6ca9b4ef1e998437e7219c45766c61ed6e9d8b8f
Sumit630 <135491660+Sumit630@users.noreply.github.com>
2024-08-20 11:07:15 +0530

    payment loan  tools

--- lib/country_scrren/view/emi_loan_guide_page.dart
+++ lib/country_scrren/view/emi_loan_guide_page.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:loan_guru/country_scrren/view/crriticism_and_cons_scrren/view/criticism_and_cons_page.dart';
 import 'package:loan_guru/country_scrren/view/interest_rates_loans_scrren/view/interest_rates_loans_introdution_page.dart';
 import 'package:loan_guru/country_scrren/view/loan_guide_scrren/view/loan_guide_page.dart';
+import 'package:loan_guru/country_scrren/view/payday_loan_alternatives_scrren/view/payday_loan_alternatives_page.dart';
+import 'package:loan_guru/country_scrren/view/payday_loan_scrren/contrlloer/pay_day_controller.dart';
+import 'package:loan_guru/country_scrren/view/payday_loan_scrren/view/pay_day_page.dart';
+import 'package:loan_guru/country_scrren/view/pyaday_specirftific_scrren/view/pyaday_specific_page.dart';
 import 'package:loan_guru/country_scrren/view/tips_on_getting_loan_scrren/view/tips_on_getting_loan_page.dart';
+import 'package:loan_guru/country_scrren/view/variatuion_on_payday_scrren/view/variation_on_payday_page.dart';
+import 'package:loan_guru/country_scrren/view/why_payday_scrren/view/why_paydey_page.dart';
 import 'package:loan_guru/global_var.dart';
 import 'package:loan_guru/utils/cons.dart';
 
 import '../controller/country_controller.dart';
 import 'Repayment_scrren/view/repayment_page.dart';
 import 'all_types_of_loan_scrren/view/all_types_of_loan_page.dart';
+import 'comparisons_lenders_scrren/view/comparisons_page.dart';
 import 'currency_converter_screen/view/currency_converter_page.dart';
 import 'finance_tips_scrren/view/finance_page.dart';
 import 'insurance_policy_scrren/view/insurance_policy_page.dart';
@@ -70,15 +78,15 @@ class _EmiLoanGuidePageState extends State<EmiLoanGuidePage> {
                       },child: const Image(image: AssetImage("assets/images/home_cureency_converter.webp"))),
                       2.ph,
                       InkWell(onTap: () {
-                        nextPageFade(MutualFundPage());
+                        nextPageFade(const MutualFundPage());
                       },child: const Image(image: AssetImage("assets/images/home_mutual_funds.webp"))),
                       2.ph,
                       InkWell(onTap: () {
-                        nextPageFade(InsurancePolicyPage());
+                        nextPageFade(const InsurancePolicyPage());
                       },child: const Image(image: AssetImage("assets/images/home_insurance_policy.webp"))),
                       2.ph,
                       InkWell(onTap: () {
-                        nextPageFade(FinancePage());
+                        nextPageFade(const FinancePage());
                       },child: const Image(image: AssetImage("assets/images/home_finance_tips.webp"))),
                       2.ph,
                       Text("Other Credit Card Tools",style: TextStyle(fontWeight: FontWeight.w600,color: Colors.black,fontSize: 24.sp,fontFamily: "Regular"),),
@@ -89,19 +97,33 @@ class _EmiLoanGuidePageState extends State<EmiLoanGuidePage> {
                       2.ph,
                       Text("Payment LoanTools",style: TextStyle(fontWeight: FontWeight.w600,color: Colors.black,fontSize: 24.sp,fontFamily: "Regular"),),
                       2.ph,
-                      const Image(image: AssetImage("assets/images/home_payday_process.webp")),
+                      InkWell(onTap: () {
+                        nextPageFade(const PayDayPage());
+                      },child: const Image(image: AssetImage("assets/images/home_payday_process.webp"))),
                       2.ph,
-                      const Image(image: AssetImage("assets/images/home_why_payday.webp")),
+                      InkWell(onTap: () {
+                        nextPageFade(const WhyPaydeyPage());
+                      },child: const Image(image: AssetImage("assets/images/home_why_payday.webp"))),
                       2.ph,
-                      const Image(image: AssetImage("assets/images/home_payday_countries.webp")),
+                      InkWell(onTap: () {
+                        nextPageFade(const PyadaySpecificPage());
+                      },child: const Image(image: AssetImage("assets/images/home_payday_countries.webp"))),
                       2.ph,
-                      const Image(image: AssetImage("assets/images/home_payday_lenders.webp")),
+                      InkWell(onTap: () {
+                        nextPageFade(const ComparisonsPage());
+                      },child: const Image(image: AssetImage("assets/images/home_payday_lenders.webp"))),
                       2.ph,
-                      const Image(image: AssetImage("assets/images/home_comparisons_variation.webp")),
+                      InkWell(onTap: () {
+                        nextPageFade(const VariationOnPaydayPage());
+                      },child: const Image(image: AssetImage("assets/images/home_comparisons_variation.webp"))),
                       2.ph,
-                      const Image(image: AssetImage("assets/images/home_payday_alternatives.webp")),
+                      InkWell(onTap: () {
+                        nextPageFade(const PaydayLoanAlternativesPage());
+                      },child: const Image(image: AssetImage("assets/images/home_payday_alternatives.webp"))),
                       2.ph,
-                      const Image(image: AssetImage("assets/images/home_payday_criticism.webp")),
+                      InkWell(onTap: () {
+                        nextPageFade(const CriticismAndConsPage());
+                      },child: const Image(image: AssetImage("assets/images/home_payday_criticism.webp"))),
                     ],
                   ),
                 ),
---
+++ lib/country_scrren/view/pyaday_specirftific_scrren/view/pyaday_specific_page.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:loan_guru/utils/cons.dart';
+import 'package:scroll_loop_auto_scroll/scroll_loop_auto_scroll.dart';
+
+import '../../../../utils/color.dart';
+import '../../interest_rates_loans_scrren/controller/interest_controller.dart';
+import '../controller/paydaay_controller.dart';
+
+class PyadaySpecificPage extends StatefulWidget {
+  const PyadaySpecificPage({super.key});
+
+  @override
+  State<PyadaySpecificPage> createState() => _PyadaySpecificPageState();
+}
+
+class _PyadaySpecificPageState extends State<PyadaySpecificPage> {
+  @override
+  Widget build(BuildContext context) {
+    MediaQueryData mediaQueryData = MediaQuery.of(context);
+    double screenWidth = mediaQueryData.size.width;
+    double screenHeight = mediaQueryData.size.height;
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Container(
+              height: screenHeight / 12.8,
+              width: screenWidth,
+              decoration: const BoxDecoration(
+                gradient:
+                LinearGradient(colors: [colorPink, colorRed]),
+              ),
+              alignment: Alignment.center,
+              child: Column(crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(15),
+                        child: InkWell(onTap: () {
+                          Navigator.pop(context);
+                        },child: const Icon(Icons.arrow_back_ios,color: Colors.white,)),
+                      ),
+                      Expanded(
+                        child: ScrollLoopAutoScroll(
+                          duration: const Duration(minutes: 4),
+                          // delayAfterScrollInput: Duration(seconds: 50),
+                          scrollDirection: Axis.horizontal,
+                          child: Text(
+                            "Payday In Specific Countries",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontFamily: "Regular",
+                                color: Colors.white,
+                                fontSize: 24.sp),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+
+                ],
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(12),
+                child: Column(crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Container(
+                      decoration:  BoxDecoration(
+                        color:colorDimRed,
+                        border: Border.all(color: textDimColor,width: 2),
+                        borderRadius: BorderRadius.all(Radius.circular(12)),
+                      ),
+                      alignment: Alignment.center,
+                      child:  Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Text("${InterestController.interestList}",textAlign: TextAlign.center,style: TextStyle(color: textBleckColor,fontWeight: FontWeight.w600,fontSize: 20,fontFamily: "Regular"),),
+                      ),
+                    ),
+                    2.ph,
+                    Expanded(
+                      child: Container(
+                        decoration:  BoxDecoration(
+                          color:colorDimRed,
+                          border: Border.all(color: textDimColor,width: 2),
+                          borderRadius: BorderRadius.all(Radius.circular(12)),
+                        ),
+                        alignment: Alignment.center,
+                        child: SingleChildScrollView(
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.all(10),
+                                child: Text("${PaydaaySpecificController.payDayString}",textAlign: TextAlign.center,style: TextStyle(color: textBleckColor,fontWeight: FontWeight.w600,fontSize:20,fontFamily: "Regular"),),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
